284544e3ce1880e9e55c2c629dad132614694d2a
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-13 08:49:18 +0100

    Init project and add first validation

---
+++ controllers/photos.controller.js
@@ -0,0 +1,63 @@
+const Photo = require('../models/photo.model');
+
+/****** SUBMIT PHOTO ********/
+
+exports.add = async (req, res) => {
+  try {
+    const { title, author, email } = req.fields;
+    const file = req.files.file;
+
+    if (title && author && email && file) {
+      const fileName = file.path.split('/').slice(-1)[0];
+      const fileExt = fileName.split('.').slice(-1)[0];
+
+      if (
+        title.length <= 25 &&
+        author.length <= 50 &&
+        (fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif')
+      ) {
+        const newPhoto = new Photo({
+          title,
+          author,
+          email,
+          src: fileName,
+          votes: 0,
+        });
+        await newPhoto.save();
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong input!');
+      }
+    } else {
+      throw new Error('Wrong input!');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+/****** LOAD ALL PHOTOS ********/
+
+exports.loadAll = async (req, res) => {
+  try {
+    res.json(await Photo.find());
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+/****** VOTE FOR PHOTO ********/
+
+exports.vote = async (req, res) => {
+  try {
+    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    else {
+      photoToUpdate.votes++;
+      photoToUpdate.save();
+      res.send({ message: 'OK' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
